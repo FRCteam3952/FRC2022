bdf7aecfac5fdaf9a567bde935f025b2034cc32e
Sammy Hajhamid <brycepc@pepsipu.com>
2022-02-20 16:55:44 -0800

    Co-authored-by: bobandjoe <bobandjoe@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,41 +1,83 @@
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.util.*;
 
 public class Climber extends SubsystemBase {
+  // feeding system
 
-    //feeding system
-
-    private final Talon mArmTalon;
-    private final Talon mHookTalon;
-
-
+  private final Talon mArmAngleTalon;
+  private final Talon mHookTalon;
+  private final DigitalInput topLimitSwitch;
+  private final DigitalInput bottomLimitSwitch;
+  private final DigitalInput topOrBottomLimitSwitch;
 
   /** Creates a new ExampleSubsystem. */
   public Climber() {
-    mArmTalon = new Talon(8);
+    mArmAngleTalon = new Talon(8);
     mHookTalon = new Talon(7);
+    topLimitSwitch = new DigitalInput(1); 
+    bottomLimitSwitch = new DigitalInput(2);
+    topOrBottomLimitSwitch = new DigitalInput(3); //only used if third limit switch is used; not used if using manual control
   }
 
+  /* public void setMotorSpeed(double speed) {
+    if (speed < 0) {
 
-  private void setArmHeight(double height) {
-    mArmTalon.set(height);
+
+      if (topLimitSwitch.get()) {
+        mHookTalon.set(0); 
+        mArmAngleTalon.set(0);
+        if (bottomLimitSwitch.get()) {
+             mHookTalon.set(0);
+        } else {
+          mHookTalon.set(speed);
+          mArmAngleTalon.set(speed);
+         }
+
+
+          
+      } else {
+        mHookTalon.set(speed);
+      }
+    }
+  }
+  */
+
+  public void slideHook(double speed) {
+    mHookTalon.set(speed);
   }
 
-  private void setHookDisplacement(double displacement) {
-      
-}
+  public void changeArmAngle(double speed) {
+    mArmAngleTalon.set(speed);
+  }
 
-// logic to be continues TODO
+  public boolean topLimitPressed() {
+    return topLimitSwitch.get();
+  }
+
+  public boolean bottomLimitPressed() {
+    return bottomLimitSwitch.get();
+  }
+
+  public boolean topOrBottomLimitPressed() {
+    return topOrBottomLimitSwitch.get();
+  }
+
+  // logic to be continues TODO
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
   }
 
   @Override
@@ -43,5 +85,4 @@ public class Climber extends SubsystemBase {
     // This method will be called once per scheduler run during simulation
   }
 
-  
 }
