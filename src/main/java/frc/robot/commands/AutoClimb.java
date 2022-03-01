ed62cb582a92639190059703be37c6b3e7252f36
Addison Chan <addison@abcis.me>
2022-02-28 16:01:46 -0800

    hi

--- src/main/java/frc/robot/commands/AutoClimb.java
+++ src/main/java/frc/robot/commands/AutoClimb.java
@@ -7,7 +7,7 @@ package frc.robot.commands;
 import frc.robot.subsystems.Climber;
 import frc.robot.RobotContainer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-
+// this does climb auto
 public class AutoClimb extends CommandBase {
   private final Climber climber;
   /**
