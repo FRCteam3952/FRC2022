dc55268f0e431ce1f8cb009a3486a6fad7c4f45b
Max Chen <bobandjoe132@gmail.com>
2022-03-02 16:38:58 -0800

    add framework for index

--- src/main/java/frc/robot/commands/IndexBalls.java
+++ src/main/java/frc/robot/commands/IndexBalls.java
@@ -4,19 +4,19 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.Loader;
+import frc.robot.subsystems.Indexer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class LoadBalls extends CommandBase {
-  private final Loader ingest;
+public class IndexBalls extends CommandBase {
+  private final Indexer index;
   private double power;
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public LoadBalls(Loader subsystem) {
-    ingest = subsystem;
+  public IndexBalls(Indexer subsystem) {
+    index = subsystem;
     power = 0.5;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
@@ -25,19 +25,19 @@ public class LoadBalls extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-      ingest.setLoadingRollerSpeed(0);
+
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    ingest.setLoadingRollerSpeed(power);
+    
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    ingest.setLoadingRollerSpeed(0);
+
   }
 
   // Returns true when the command should end.
