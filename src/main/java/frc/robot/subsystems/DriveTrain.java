8f20e642c7ea1276465d4f10d7a5583160c8e9d6
Sean Son <40509013+SeanSon2005@users.noreply.github.com>
2022-03-29 15:37:02 -0700

    most updated code

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -88,35 +88,7 @@ public class DriveTrain extends SubsystemBase {
     return settingDistance;
   }
 
-  public void setMecanumDrive(double translationAngle, double translationPower, double turnPower)
-{
-    // calculate motor power
-    double ADPower = translationPower * Math.sqrt(2) * 0.5 * (Math.sin(translationAngle) + Math.cos(translationAngle));
-    double BCPower = translationPower * Math.sqrt(2) * 0.5 * (Math.sin(translationAngle) - Math.cos(translationAngle));
 
-    // System.out.println( "front left: " + frontLeft.isFollower() + " front right " + frontRight.isFollower() + " rear left " + rearLeft.isFollower() + " rear right " + rearRight.isFollower());
-    // check if turning power will interfere with normal translation
-    // check ADPower to see if trying to apply turnPower would put motor power over 1.0 or under -1.0
-    double turningScale = Math.max(Math.abs(ADPower + turnPower), Math.abs(ADPower - turnPower)); 
-    // check BCPower to see if trying to apply turnPower would put motor power over 1.0 or under -1.0
-    turningScale = Math.max(turningScale, Math.max(Math.abs(BCPower + turnPower), Math.abs(BCPower - turnPower)));
-
-    // adjust turn power scale correctly
-    if (Math.abs(turningScale) < 1.0)
-    {
-        turningScale = 1.0;
-    }
-
-    // set the motors, and divide them by turningScale to make sure none of them go over the top, which would alter the translation angle
-//     frontLeft.set((ADPower - turningScale) / turningScale);
-//     rearLeft.set((BCPower - turningScale) / turningScale);
-//     frontRight.set((BCPower + turningScale) / turningScale);
-//     rearRight.set((ADPower + turningScale) / turningScale);
-System.out.println((ADPower - turningScale) / turningScale);
-System.out.println((BCPower - turningScale) / turningScale);
-System.out.println((BCPower + turningScale) / turningScale);
-System.out.println((ADPower + turningScale) / turningScale);
-  }
 
   @Override
   public void periodic() {
