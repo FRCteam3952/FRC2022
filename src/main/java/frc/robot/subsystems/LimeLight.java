6fc184e20d3f7e95b33fe651584dc4cd849d01ca
AC <addison@abcis.me>
2022-04-15 16:02:16 -0700

    Revert "clen"
    This reverts commit d58915cfb7abb3778fc52a777a93246ff28e4bed.

--- src/main/java/frc/robot/subsystems/LimeLight.java
+++ src/main/java/frc/robot/subsystems/LimeLight.java
@@ -6,42 +6,33 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.math.controller.PIDController;
 
-public class Limelight extends SubsystemBase {
-    private static NetworkTableInstance inst;
-    private static NetworkTable table;
+public class LimeLight extends SubsystemBase{
+    private NetworkTableInstance inst;
+    private NetworkTable table;
     private PIDController pidcontrol;
     private final float kp = 0.05f;
     private final float ki = 0.01f;
     private final float kd = 0.02f;
 
-    public static double limelightAngleDeg = 30;
-    public static double limelightHeightInch = 29;
-    public static double goalHeightInch = 104;
-
-    public Limelight(){
+    public LimeLight(){
         pidcontrol = new PIDController(1, ki, kd);
         inst = NetworkTableInstance.getDefault();
         table = inst.getTable("limelight"); //initiate limelight table
     }
-
     public double getAdjustment(){
         float tx = table.getEntry("tx").getNumber(0).floatValue() * kp; //get target x position
         double steering_adjust = pidcontrol.calculate(tx); //calculate PID
         return steering_adjust;
     }
-    
-    public static double distanceToHoop() {
-        turnOnLED();
-        double targetOffsetAngleVert = table.getEntry("ty").getDouble(0.0);
-        double angletoGoalDeg = limelightAngleDeg + targetOffsetAngleVert;
-        double angletoGoalRad = angletoGoalDeg * (Math.PI / 180);
-        turnOffLED();
-        return (goalHeightInch - limelightHeightInch) / Math.tan(angletoGoalRad) * 0.0254; //returns in meters
-  }
-    public static void turnOnLED(){
+    public double getDistance(){
+        double ty = table.getEntry("ty").getNumber(0).doubleValue(); //get target height
+        double distance = ty*0.2 - 20; //convert target height into distance estimate
+        return distance;
+    }
+    public void turnOnLED(){
         table.getEntry("ledMode").setDouble(3);
     }
-    public static void turnOffLED(){
+    public void turnOffLED(){
         table.getEntry("ledMode").setDouble(1);
     }
     @Override
